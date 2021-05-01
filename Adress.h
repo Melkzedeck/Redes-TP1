60da6b53fa80d506f8733789d4f27492551c775d
MZM <49241786+Melkzedeck@users.noreply.github.com>
2021-04-30 23:11:48 -0300

    Update Adress.h
    Add comentários e documentações

--- Adress.h
+++ Adress.h
@@ -1,20 +1,24 @@
 #pragma once
+//----------- Bibliotecas interface POSIX -----------//
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
+//----------------------////----------------------//
 #include <stdio.h>
 #include <unistd.h>
 #include <string> /* memset() */
 #include <string.h>
 #include <iostream>   // std::cout
-#include <stdexcept>
+#include <stdexcept> //Tratamento de algumas excecoes
 
-void logexit(const char *msg);
+void logexit(const char *msg); //saida de erro
 void usage_s(std::string name);
 void usage_c(std::string name);
 
+/* Classe para tratamento de endereco e porta. A implementacao é baseada 
+na interface POSIX (desenvolvida nessa classe em C) de soquetes de rede*/
 
 class Adress {
 	uint16_t port_;
@@ -25,17 +29,26 @@ class Adress {
 	socklen_t *addrlen_;
 	int family_;
 public:
+	//Construtor da classe usando a versão(char) e a porta(string) como argumento
 	Adress(char v, std::string port_s);
+	//Construtor usando o endereco(string) e porta(string)
 	Adress(std::string addrstr, std::string port_s);
+	//Construtor usando ponteiro do struct sockaddr
 	Adress(struct sockaddr* addr);
+	//Construtor copia
 	Adress(const Adress&);
+	//----------- Funcoes GET -----------//
 	uint16_t port()const {return port_;};
 	const struct sockaddr* addr() const {return addr_;};
 	const socklen_t* addrlen() const {return addrlen_;};
+	//----------- Termino das funcoes GET -----------//
 	int family() const {return family_;};
+	//retorna o endereco no formato string
 	std::string str() const;
-	void refresh();
+	void refresh();//Por enquanto não há necessidade dessa funcao. Mas pode ser util
+	//operador de igualdade 
 	Adress& operator=(const Adress&);
+	//Destrutor (alocacao de memoria)
 	~Adress();
 };
 
